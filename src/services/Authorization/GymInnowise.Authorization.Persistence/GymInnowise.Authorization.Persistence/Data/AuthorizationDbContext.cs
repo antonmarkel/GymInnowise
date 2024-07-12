5aa16c3fa63b4219a84d0e85b965056120b1b6db
AntonMarkelau <anton.markelau@innowise.com>
2024-07-12 01:57:01 -0700

    Configure db context and entities

---
+++ src/services/Authorization/GymInnowise.Authorization.Persistence/GymInnowise.Authorization.Persistence/Data/AuthorizationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using GymInnowise.Authorization.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymInnowise.Authorization.Persistence.Data
+{
+    public class AuthorizationDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+
+        public AuthorizationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=Student;Username=postgres;Password=MADL");
+        }
+
+    }
+}
---
+++ src/services/Authorization/GymInnowise.Authorization.Persistence/GymInnowise.Authorization.Persistence/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymInnowise.Authorization.Persistence.Models
+{
+    public class Account
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Email { get;set; }
+        [Required]
+        public string PhoneNumber { get; set;}
+        public string PasswordHash { get; set;}
+        public DateTime CreatedDate { get;set; }
+        public DateTime ModifiedDate { get;set; }
+
+        public List<Role> Roles { get; set; }
+
+    }
+}

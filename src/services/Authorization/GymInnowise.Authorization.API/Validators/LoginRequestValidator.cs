4fd1e04f86fff0be66700f9359300f261c5c7748
AntonMarkelau <anton.markelau@innowise.com>
2024-07-23 02:44:43 -0700

    Refactor validators

--- src/services/Authorization/GymInnowise.Authorization.API/Validators/LoginRequestValidator.cs
+++ src/services/Authorization/GymInnowise.Authorization.API/Validators/LoginRequestValidator.cs
@@ -1,26 +1,16 @@
 ﻿using FluentValidation;
+using GymInnowise.Authorization.API.Extensions;
 using GymInnowise.Authorization.Shared.Dtos.RequestModels;
 
 namespace GymInnowise.Authorization.API.Validators
 {
     public class LoginRequestValidator : AbstractValidator<LoginRequest>
     {
-        private const int PasswordMinLength = 8, PasswordMaxLength = 16;
-
         public LoginRequestValidator()
         {
-            RuleFor(rq => rq.Email).Cascade(CascadeMode.Stop)
-                .NotEmpty()
-                .WithMessage("Email address cannot be empty!")
-                .EmailAddress()
-                .WithMessage("Invalid email!");
-            RuleFor(rq => rq.Password)
-                .NotEmpty()
-                .WithMessage("Your password cannot be empty!")
-                .MinimumLength(PasswordMinLength)
-                .WithMessage($"Your password length must be at least {PasswordMinLength}!")
-                .MaximumLength(PasswordMaxLength)
-                .WithMessage($"Your password length must not exceed {PasswordMaxLength}!");
+            RuleFor(rq => rq.Email).EmailAddress()
+                .WithMessage("Invalid email address!");
+            RuleFor(rq => rq.Password).Password();
         }
     }
 }
---
+++ src/services/Authorization/GymInnowise.Authorization.API/Validators/RefreshRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using GymInnowise.Authorization.API.Extensions;
+using GymInnowise.Authorization.Shared.Dtos.RequestModels;
+
+namespace GymInnowise.Authorization.API.Validators
+{
+    public class RefreshRequestValidator : AbstractValidator<RefreshRequest>
+    {
+        public RefreshRequestValidator()
+        {
+            RuleFor(rq => rq.RefreshToken).RefreshToken();
+        }
+    }
+}

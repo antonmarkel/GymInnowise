1c1ac3147213ab0dbbd6a5d65967d73897810a52
AntonMarkelau <anton.markelau@innowise.com>
2024-08-29 07:04:34 -0700

    Add configuration

---
+++ src/services/UserService/GymInnowise.UserService.Configuration/Data/DbSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace GymInnowise.UserService.Configuration.Data
+{
+    public class DbSettings
+    {
+        public string Server { get; set; } = string.Empty;
+        public string Database { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int Port { get; set; }
+    }
+}

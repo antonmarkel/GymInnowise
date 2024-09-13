9b7fd80158f65e6ddb00569b3693892141d800ea
AntonMarkelau <anton.markelau@innowise.com>
2024-09-13 05:27:43 -0700

    Add models

---
+++ src/services/GymService/GymInnowise.GymService.Persistence/Models/Entities/BlockingEventEntity.cs
@@ -0,0 +1,14 @@
+﻿
+namespace GymInnowise.GymService.Persistence.Models.Entities
+{
+    public class BlockingEventEntity
+    {
+        public Guid Id { get; set; }
+        public Guid GymId { get; set; }
+        public Guid CreatedBy { get; set; }
+        public string Reason { get; set; } = string.Empty;
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ src/services/GymService/GymInnowise.GymService.Persistence/Models/Entities/GymEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using GymInnowise.GymService.Shared.Enums;
+
+namespace GymInnowise.GymService.Persistence.Models.Entities
+{
+    public class GymEntity
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string ContactInfo { get; set; }
+        public GymUsageType UsageType { get; set; }
+        public int MaxOccupancy { get; set; }
+        public float SquareFootage { get; set; }
+        public TimeSpan OpenTime { get; set; }
+        public TimeSpan CloseTime { get; set; }
+        public byte DaysAvailableMask { get; set; }
+        public GymPayType PayType { get; set; }
+        public decimal CostValue { get; set; }
+        public List<BlockingEventEntity> BlockingEvents { get; set; } = [];
+    }
+}

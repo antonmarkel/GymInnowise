581f14aac4bcbaba39334a0ee26951d97183c8e9
AntonMarkelau <anton.markelau@innowise.com>
2024-09-23 07:39:07 -0700

    Add authorization update account functional

---
+++ src/services/Authorization/GymInnowise.Authorization.API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using GymInnowise.Authorization.Logic.Interfaces;
+using GymInnowise.Authorization.Shared.Dtos.RequestModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymInnowise.Authorization.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterRequest registerRequest)
+        {
+            var registerResult = await _accountService.RegisterAsync(registerRequest);
+
+            return registerResult.Match<IActionResult>(
+                _ => Created(),
+                _ => BadRequest("Account with this email or mobile phone already exists! Try to log in!")
+            );
+        }
+
+        [Authorize]
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateRequest updateRequest)
+        {
+            var updateResult = await _accountService.UpdateAsync(id, updateRequest);
+
+            return updateResult.Match<IActionResult>(
+                _ => NoContent(),
+                _ => NotFound()
+            );
+        }
+
+        public async Task<IActionResult> UpdateRolesAsync(Guid id, [FromBody] UpdateRolesRequest updateRolesRequest)
+        {
+            var updateResult = await _accountService.UpdateRolesAsync(id, updateRolesRequest);
+
+            return updateResult.Match<IActionResult>(
+                _ => NoContent(),
+                _ => NotFound()
+            );
+        }
+    }
+}
--- src/services/Authorization/GymInnowise.Authorization.Shared/Authorization/Roles.cs
+++ src/services/Authorization/GymInnowise.Authorization.Shared/Authorization/Roles.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection;
+
 namespace GymInnowise.Authorization.Shared.Authorization
 {
     public static class Roles
@@ -7,5 +9,16 @@ namespace GymInnowise.Authorization.Shared.Authorization
         public const string Coach = "Coach";
         public const string Admin = "Admin";
         public const string CoachOrAdmin = "Coach,Admin";
+
+        public static readonly HashSet<string> AllRoles = GetAllRoles();
+
+        private static HashSet<string> GetAllRoles()
+        {
+            return typeof(Roles)
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(string))
+                .Select(fi => (string)fi.GetRawConstantValue()!)
+                .ToHashSet();
+        }
     }
 }

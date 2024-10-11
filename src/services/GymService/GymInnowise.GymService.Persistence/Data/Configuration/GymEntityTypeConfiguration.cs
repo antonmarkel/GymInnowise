75f501c80731262f8d81e7ac0104c3d514988d95
AntonMarkelau <anton.markelau@innowise.com>
2024-10-11 05:58:37 -0700

    Extract entity configuration

---
+++ src/services/GymService/GymInnowise.GymService.Persistence/Data/Configuration/GymEntityTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using GymInnowise.GymService.Persistence.Models.Entities;
+using GymInnowise.Shared.Gym.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Reflection.Emit;
+using Newtonsoft.Json;
+
+namespace GymInnowise.GymService.Persistence.Data.Configuration
+{
+    public class GymEntityTypeConfiguration : IEntityTypeConfiguration<GymEntity>
+    {
+        public void Configure(EntityTypeBuilder<GymEntity> builder)
+        {
+            builder.HasKey(g => g.Id);
+            builder.Property(g => g.Name).HasMaxLength(100).IsRequired();
+            builder.Property(g => g.Address).HasMaxLength(200).IsRequired();
+            builder.Property(g => g.ContactInfo).HasMaxLength(200).IsRequired();
+            builder.Property(g => g.UsageType).HasConversion<string>().IsRequired();
+            builder.Property(g => g.PayType).HasConversion<string>().IsRequired();
+            builder.Property(g => g.Tags).HasConversion(GetGymTagConverter());
+        }
+
+        private static ValueConverter<List<GymTag>, string> GetGymTagConverter()
+        {
+            return new ValueConverter<List<GymTag>, string>(
+                v => JsonConvert.SerializeObject(
+                    v.Select(v => v.ToString())),
+                v => JsonConvert.DeserializeObject<List<string>>(v)!
+                    .Select(s => (GymTag)Enum.Parse(typeof(GymTag), s)).ToList());
+        }
+    }
+}

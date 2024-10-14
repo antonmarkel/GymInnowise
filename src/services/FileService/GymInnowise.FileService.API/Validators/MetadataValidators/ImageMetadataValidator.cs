df698e14e5bee23527ebd3aea5799365facfde0d
AntonMarkelau <anton.markelau@innowise.com>
2024-10-14 07:05:47 -0700

    Add metadata validators

---
+++ src/services/FileService/GymInnowise.FileService.API/Validators/MetadataValidators/ImageMetadataValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using GymInnowise.FileService.Configuration.Blob;
+using GymInnowise.Shared.Files.Dtos.Metadata;
+using Microsoft.Extensions.Options;
+
+namespace GymInnowise.FileService.API.Validators.MetadataValidators
+{
+    public class ImageMetadataValidator : AbstractValidator<ImageMetadata>
+    {
+        private readonly FileSettings _settings;
+
+        public ImageMetadataValidator(IOptions<FileSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public ImageMetadataValidator()
+        {
+            RuleFor(metadata => metadata.Format).Must(format => _settings!.ImageAllowedExtensions.Contains(format))
+                .WithMessage("This format is not allowed");
+        }
+    }
+}
